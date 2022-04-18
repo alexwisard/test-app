583ee723102a2439b6efe13af60749805d19f9a2
Oleksandr Petrusha <alexander.petrusha@gmail.com>
2022-04-18 21:05:23 +0300

    react-events

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -1,11 +1,14 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment } from 'react';
+import './AppFooter.css';
 
 export default class AppFooter extends Component {
+
     render() {
+        const currentYear = new Date().getFullYear();
         return (
             <Fragment>
                 <hr />
-                <p>Copyright &copy 2022 Alexwisard</p>
+                <p className="footer">Copyright &copy; 2020 - { currentYear} Acme Ltd.</p>
             </Fragment>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import AppHeader from './AppHeader';
 import AppFooter from './AppFooter';
+import AppContent from './AppContent';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './index.css';
+
 class App extends Component {
-  render(){
-    return <giv>
-      <div>
-        <h1>Hello, World!</h1>
+
+  render() {
+    const myProps = {
+      title: "My Cool App!",
+      subject: "My subject",
+      favourite_color: "red",
+    }
+
+    return (
+      <div className="app">
+        <AppHeader {...myProps}  />
+        <AppContent />
+        <AppFooter />
       </div>
-      <AppFooter />
-    </giv>
+    );
   }
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
+ReactDOM.render(<App />, document.getElementById('root'));
