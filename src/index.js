25c7023dfb0e8ca9f30b2fa6fc35a0fc346aa51d
Oleksandr Petrusha <alexander.petrusha@gmail.com>
2022-04-18 13:52:44 +0300

    Update AppFooter.js and index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import AppFooter from './AppFooter';
+class App extends Component {
+  render(){
+    return <giv>
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+      <AppFooter />
+    </giv>
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +19,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
